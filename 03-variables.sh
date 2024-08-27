1575083a02154eb822cf731fe11584a3d1dbd32b
narendra <narendra.devops1144@gmail.com>
2024-08-27 23:31:39 +0530

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Narendra
+PERSON2=Ram
+
+echo "$PERSON1:: Hi ${PERSON2}, how are you?"
+echo "${PERSON2}:: Hi $PERSON1, i am good, how are you doing?"
+echo "$PERSON1:: nothing ${PERSON2}, as usual it's going on, any new updates from your end?"
+echo "${PERSON2}:: i started learing aws devops "
