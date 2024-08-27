48d7646045570ee8d978080b2ed22df9ace907ec
narendra <narendra.devops1144@gmail.com>
2024-08-27 23:22:05 +0530

    added conversation script

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Narendra:: Hi Ram, how are you?"
+echo "Ram:: Hi Narendra, i am good, how are you doing?"
+echo "Narendra:: nothing Ram, as usual it's going on, any new updates from your end?"
+echo "Ram:: i started learing aws devops "
