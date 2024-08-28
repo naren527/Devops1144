9a19b42fd5894b306e7e46d6d4467c0a46428215
narendra <narendra.devops1144@gmail.com>
2024-08-28 15:44:09 +0530

    showing date and time

---
+++ 10-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Number=$1
+
+if [ $Number -gt 20 ]
+  then 
+
+      echo "given number: $Number is greater than 20"
+
+      else 
+        echo "given number: $Number is less than 20"
+
+        fi
