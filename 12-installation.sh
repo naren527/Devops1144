de3ce0531a6903bbaadd510a69d11c532672b59d
narendra <narendra.devops1144@gmail.com>
2024-08-29 16:03:33 +0530

    installing git

--- 12-installation.sh
+++ 12-installation.sh
@@ -11,4 +11,13 @@ then
     exit 1
 fi
 
-dnf install git -y 
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then 
+  echo "git is not installed, do install it"
+   dnf install git -y
+
+   else 
+   echo "git is already istalled, no worries"
+fi   
