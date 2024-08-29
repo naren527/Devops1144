6949417382095737949597c97c7e0da1166e03b0
narendra <narendra.devops1144@gmail.com>
2024-08-29 15:41:47 +0530

    installing git

--- 12-installation.sh
+++ 12-installation.sh
@@ -3,4 +3,11 @@
 
 USERID=$(id -u)
 
-echo "user id is :$USERID"
+#echo "user id is :$USERID"
+
+if [$USERID -ne 0]
+then 
+    echo "plese run this script through root previlages"
+fi
+
+dnf install git -y 
